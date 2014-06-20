260de64f77f21de009d2ed76ee66fe92260e754f
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-06-20 04:05:06 -0300

    Un icono en la barra de tareas

--- Resources/app.js
+++ Resources/app.js
@@ -1,4 +1,19 @@
-App = Ember.Application.create({});
+logger = Ember.Logger;
+
+App = Ember.Application.create({
+    tray: null,
+    ready: function() {
+        logger.log("app is ready");
+        var me = this;
+        var tray = Ti.UI.addTray("app://app_logo.png", function() {
+            me.trayClicked();
+        });
+        this.set("tray", tray);
+    },
+    trayClicked: function() {
+        logger.log("tray icon clicked");
+    }
+});
 
 App.Router.map(function() {
     this.resource('post', {
@@ -13,53 +28,5 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
     activate: function() {},
-    model: function() {
-        return {
-            village_people: [{
-                name: "sam",
-                age: 34
-            }, {
-                name: "susan",
-                age: 32
-            }, {
-                name: "paul",
-                age: 41
-            }]
-        };
-    }
-});
-
-App.IndexController = Ember.ObjectController.extend({
-    toto: function() {
-        alert("TOTO");
-    },
-    Accordion: JQ.Accordion.extend({
-        resize_well: function() {
-            var h = this.parent_height;
-            // this.$().css({
-            //     height: (h - 22) + "px",
-            // });
-        }.observes("parent_height")
-    }),
-    FileButton: JQ.Button.extend({
-        label: "File"
-    }),
-    EditButton: JQ.Button.extend({
-        label: "Edit"
-    }),
-    HelpButton: JQ.Button.extend({
-        label: "Help"
-    })
-})
-
-EUI = Ember.Namespace.create();
-EUI.Layout = Ember.View.extend({
-    tagName: "div",
-    classNames: ['n-full-size'],
-    didInsertElement: function() {
-        this.$().layout({
-            fit: true
-        });
-        //$.parser.parse();
-    }
+    model: function() {}
 });
